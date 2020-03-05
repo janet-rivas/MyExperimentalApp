1bd670b886ce4f977fc6ad9dcc48934225f0eaf1
Janet Rivas <janet.rivas.mendoza@officedepot.com>
2020-03-05 12:36:24 -0500

    Adding Redux Example

---
+++ App.js
@@ -0,0 +1,108 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
+
+import {Colors} from 'react-native/Libraries/NewAppScreen';
+import {createStore} from 'redux';
+import CounterApp from './src/CounterApp';
+import {Provider} from 'react-redux';
+
+/**
+ * Store - holds our state - THERE IS ONLY ONE STATE
+ * Action - State can be modified using actions - SIMPLE OBJECTS
+ * Dispatcher - Action needs to be sent by someone - known as dispatching an action
+ * Reducer - receives the action and modifies the state to give us a new state
+ *  - pure functions
+ *  - only mandatory argument is the 'type'
+ * Subscriber - listens for state change to update the ui
+ */
+const initialState = {
+  counter: 0,
+};
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case 'INCREASE_COUNTER':
+      return {counter: state.counter + 1};
+    case 'DECREASE_COUNTER':
+      return {counter: state.counter - 1};
+  }
+  return state;
+};
+
+const store = createStore(reducer);
+const App: () => React$Node = () => {
+  return (
+    <>
+      <StatusBar barStyle="dark-content" />
+      <SafeAreaView>
+        <ScrollView
+          contentInsetAdjustmentBehavior="automatic"
+          style={styles.scrollView}>
+          <Provider store={store}>
+            <CounterApp />
+          </Provider>
+        </ScrollView>
+      </SafeAreaView>
+    </>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  scrollView: {
+    backgroundColor: Colors.lighter,
+  },
+  engine: {
+    position: 'absolute',
+    right: 0,
+  },
+  body: {
+    backgroundColor: Colors.white,
+  },
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+    color: Colors.black,
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+    color: Colors.dark,
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+  footer: {
+    color: Colors.dark,
+    fontSize: 12,
+    fontWeight: '600',
+    padding: 4,
+    paddingRight: 12,
+    textAlign: 'right',
+  },
+});
+
+export default App;
